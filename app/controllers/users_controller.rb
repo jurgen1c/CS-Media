d0387ba1252b1a5ddf2bc5591fb6f1b3f48b5e49
jurgen1c <jurgen1c@gmail.com>
2020-10-27 06:06:53 -0600

    Add users views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :username, :email, :password, presence: true
+
   has_one_attached :avatar
-  has_many :followings
-  has_many :follows, through: :followings
+  has_many :followings, dependent: :destroy
+  has_many :follows, through: :followings, dependent: :destroy
+
+  
 end
